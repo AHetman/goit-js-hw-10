e3c97a2bfc5c2f429575c929489d9fe3b43d9621
AHetman <101942183+AHetman@users.noreply.github.com>
2024-02-09 15:58:01 +0200

    update

---
+++ src/js/2-snackbar.js
@@ -0,0 +1,56 @@
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+const delayInput = document.querySelector('input[name="delay"]');
+const button = document.querySelector('button');
+
+button.addEventListener('click', onCreateNotification);
+
+function onCreateNotification(event) {
+  const radioSelector = document.querySelector('input[name="state"]:checked');
+  event.preventDefault();
+  const delay = parseInt(delayInput.value);
+  if (delayInput.value === '') {
+    iziToast.error({
+      message: '❌ Please enter a delay value',
+      backgroundColor: 'red',
+      messageColor: 'white',
+      position: 'topRight',
+      icon: null,
+    });
+    return;
+  }
+
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (radioSelector.value === 'fulfilled') {
+        resolve();
+      } else {
+        reject();
+      }
+    }, delay);
+  });
+
+  promise
+    .then(() => {
+      console.log(`✅ Fulfilled promise in ${delay}ms`);
+      iziToast.success({
+        message: `✅ Fulfilled promise in ${delay}ms`,
+        backgroundColor: 'green',
+        messageColor: 'white',
+        position: 'topRight',
+        icon: null,
+      });
+    })
+    .catch(() => {
+      console.log(`❌ Rejected promise in ${delay}ms`);
+      iziToast.error({
+        message: `❌ Rejected promise in ${delay}ms`,
+        backgroundColor: 'red',
+        messageColor: 'white',
+        position: 'topRight',
+        icon: null,
+      });
+    });
+  delayInput.value = '';
+}
